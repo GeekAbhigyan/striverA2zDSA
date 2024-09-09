f7a8e5cbde85bde308f30421ce0ede79d5da76fb
Abhigyan <singhdefinite12@gmail.com>
2024-09-09 15:32:08 +0530

    initiated

---
+++ test.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+#ifndef ONLINE_JUDGE
+	//for getting input from the input.txt
+	freopen("input.txt","r",stdin);
+
+	//for writing output to output.txt
+	freopen("output.txt","w",stdout);
+#endif
+
+	int n;
+	cin>>n;
+	cout<<n<<endl;
+	int arr[n]= {1,2,3,4,5,6,7,8,9,10};
+	
+	for(int i=0;i<10;i++){
+		cout<<arr[i]<<endl;
+	}
+
+
+
+
+
+
+}
