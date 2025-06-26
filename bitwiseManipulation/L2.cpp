cd9b6c574a5ae57f4a988a6a22a5aa69f385c20b
Abhigyan <singhdefinite12@gmail.com>
2025-06-26 15:40:21 +0530

    reversed

--- bitwiseManipulation/L2.cpp
+++ bitwiseManipulation/L2.cpp
@@ -1,11 +1,20 @@
 #include<bits/stdc++.h>
 using namespace std;
+// swap without the third variable 
 
 int main(){
+    int a =5 ;
+    int b= 10 ;
 
-	int n;
-	cin>>n;
-	cout<<n<<endl;
+    cout <<"orignal "<<endl;
+    cout<<a<<b<<endl;
 
+    a = a ^ b ;
+    b = a^ b ;
+    a = a^ b ;
+
+    cout << "reversed "<<endl;
+    cout<<a<<b<<endl;
+    return 0;
 
 }
